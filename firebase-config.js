3578933699c3432734fd3d51ad8a06dfe3a9bfec
wsiprianni <wsiprianni@icloud.com>
2026-07-29 16:15:15 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCuygY9J04xd04V1j8Pm63WOKHR4KQQB1w",
+  authDomain: "controle-de-gases-c0d76.firebaseapp.com",
+  projectId: "controle-de-gases-c0d76",
+  storageBucket: "controle-de-gases-c0d76.firebasestorage.app",
+  messagingSenderId: "946774858699",
+  appId: "1:946774858699:web:c9df886b4001ec2bcef44e",
+  measurementId: "G-YNRJLKXB4R"
+};
